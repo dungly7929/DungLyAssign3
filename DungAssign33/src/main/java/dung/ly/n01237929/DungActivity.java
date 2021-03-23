0aa8db11795dbfda32e3ba3162b55cfbcf9a05b2
dungly7929 <danks76429@gmail.com>
2021-03-22 20:10:38 -0400

    Bottom Navigation Create Commit !

--- DungAssign33/src/main/java/dung/ly/n01237929/DungActivity.java
+++ DungAssign33/src/main/java/dung/ly/n01237929/DungActivity.java
@@ -1,18 +1,47 @@
 //DUNG LY       ID: N01327929
 package dung.ly.n01237929;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DungActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DungActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView botnav = findViewById(R.id.bottom_nav);
+        botnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                switch (item.getItemId())
+                {
+                    case R.id.acttab1:
+
+                    break;
+
+                    case R.id.acttab2:
+
+                    break;
+
+                    case R.id.acttab3:
+
+                    break;
+                }
+                return true;
+            }
+        });
     }
+
 
 
 }
